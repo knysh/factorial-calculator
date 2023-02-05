72eae99591e587f79969c10096848fddefe1d4f1
Sergey Knysh <se.knysh@gmail.com>
2023-02-05 15:35:41 +0300

    init playwright project with allure report

---
+++ playwright.config.ts
@@ -0,0 +1,49 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+
+  fullyParallel: true,
+  retries: 0,
+  workers: 2,
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+    ['allure-playwright', {
+      detail: false,
+      suiteTitle: false,
+    },]
+  ],
+  use: {
+    baseURL: 'https://qainterview.pythonanywhere.com',
+    headless: false,
+    navigationTimeout: 30 * 1000,
+    actionTimeout: 5000,
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+  ],
+
+  outputDir: 'test-results/',
+};
+
+export default config;
