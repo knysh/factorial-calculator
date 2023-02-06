588c7f647851c051df77f0ace9a88f60e5505d8c
Sergey Knysh <se.knysh@gmail.com>
2023-02-06 16:28:43 +0300

    remove test on indication

--- tests/ui/factorialPage.spec.ts
+++ tests/ui/factorialPage.spec.ts
@@ -5,22 +5,26 @@ import { test } from '../../fixtures';
 import { mockGetFactorialWith400Error, mockGetFactorialWith500Error } from '../../mocks';
 
 const data = [
-  { value: ' 1 ', expectedResult: getResultTemplate('1', '1'), },
-  { value: '170', expectedResult: getResultTemplate('170', '7.257415615307999e+306'), },
-  { value: '171', expectedResult: getResultTemplate('171', 'Infinity'), },
-  { value: '1.1', expectedResult: INTEGER_ERROR_MESSAGE, },
-  { value: '1,1', expectedResult: INTEGER_ERROR_MESSAGE, },
-  { value: '@#$&%!~a', expectedResult: INTEGER_ERROR_MESSAGE, },
-  { value: '1text1', expectedResult: INTEGER_ERROR_MESSAGE, },
-  { value: '1 1', expectedResult: INTEGER_ERROR_MESSAGE, },
-  { value: '', expectedResult: INTEGER_ERROR_MESSAGE, }
+  { value: ' 1 ', expectedResult: getResultTemplate('1', '1') },
+  { value: '170', expectedResult: getResultTemplate('170', '7.257415615307999e+306') },
+  { value: '171', expectedResult: getResultTemplate('171', 'Infinity') },
+  { value: '1.1', expectedResult: INTEGER_ERROR_MESSAGE },
+  { value: '1,1', expectedResult: INTEGER_ERROR_MESSAGE },
+  { value: '@#$&%!~a', expectedResult: INTEGER_ERROR_MESSAGE },
+  { value: '1text1', expectedResult: INTEGER_ERROR_MESSAGE },
+  { value: '1 1', expectedResult: INTEGER_ERROR_MESSAGE },
+  { value: '', expectedResult: INTEGER_ERROR_MESSAGE },
 ];
 
 test.describe('Factorial page', () => {
-  data.forEach(keyValue => {
-    test(`should display result of factorial of '${keyValue.value}'`, async ({ factorialCalcPage }) => {
-      await factorialCalcPage.calculateFactorial(keyValue.value);
-      expect(await factorialCalcPage.getResult()).toEqual(keyValue.expectedResult);
+  test('should have title', async ({ page }) => {
+    await expect(page).toHaveTitle(/Factoriall/);
+  });
+
+  data.forEach(({ value, expectedResult }) => {
+    test(`should display result of factorial of '${value}'`, async ({ factorialCalcPage }) => {
+      await factorialCalcPage.calculateFactorial(value);
+      expect(await factorialCalcPage.getResult()).toEqual(expectedResult);
     });
   });
 
@@ -29,18 +33,18 @@ test.describe('Factorial page', () => {
       url: 'none',
       name: 'Nothing happened if server error'
     });
-  
+
     await mockGetFactorialWith500Error(page);
     await factorialCalcPage.calculateFactorial('1');
     expect(await factorialCalcPage.getResult()).toEqual(SERVER_ERROR_MESSAGE);
   });
-  
+
   test('should display bad request error meesage', async ({ page, factorialCalcPage }) => {
     allure.issue({
       url: 'none',
       name: 'Nothing happened if request error'
     });
-  
+
     await mockGetFactorialWith400Error(page);
     await factorialCalcPage.calculateFactorial('-1');
     expect(await factorialCalcPage.getResult()).toEqual(BAD_REQUEST_ERROR_MESSAGE);
